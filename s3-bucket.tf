b53da0c2de523c92d14cc738e52bb706f983bbcc
Tomoki <tomoki.maeda@hashicorp.com>
2023-04-06 08:11:49 +0000

    fix s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,11 +2,10 @@
 module "s3_bucket" {
   source  = "app.terraform.io/tmaeda-hashicorp/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket_prefix = "my-s3-bucket"
   acl    = "private"
-  bucket_prefix = "on-boarding bucket"
+  
   versioning = {
     enabled = true
   }
-
 }
